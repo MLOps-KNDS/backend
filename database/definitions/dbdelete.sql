baf9d703e8aee456f90c66c7561e29b68cdd0b24
Radosław Dąbkowski <radekaadek@gmail.com>
2023-04-12 20:54:30 +0200

    Basic DB scripts (#2)
    * init
    
    * readme
    
    * enter the folder
    
    * Added config options
    
    * added docstrings
    
    * basic test table
    
    * dockerfile i cant test cuz internet
    
    * -
    
    * renaming folders and Sphinx documentation
    
    * requirements fix
    
    * dockerfile fix, readme + env variables

---
+++ database/definitions/dbdelete.sql
@@ -0,0 +1,3 @@
+-- IMPORTANT!!: When editing, compare this file with the db/dbschema.sql file
+
+DROP table IF EXISTS model;
---
+++ database/scripts/dbconnect.py
@@ -0,0 +1,57 @@
+import psycopg2
+import os
+from typing import Optional
+from dbconfig import config_env, config_ini
+
+
+def connect(config: Optional[dict] = None) -> psycopg2.extensions.connection:
+    """Connects to the database
+
+    ...
+    :param config: Database connection config, defaults to None
+    :type config: Optional[dict], optional
+    ...
+    :return: Database connection
+    :rtype: psycopg2.extensions.connection
+    """
+    # Check if config was passed
+    if config is not None:
+        conn = psycopg2.connect(**config)
+        if conn:
+            return conn
+        else:
+            raise Exception("Connection failed, check your credentials")
+
+    # Check if environment variables are set
+    env_vars_found = True
+    env_vars = ["POSTGRES_HOST", "POSTGRES_NAME",
+                "POSTGRES_USER", "POSTGRES_PASS"]
+    for var in env_vars:
+        if os.environ.get(var) is None:
+            print(f"Enviroment var {var} not set, looking for database.ini")
+            env_vars_found = False
+            break
+    if env_vars_found:
+        config = config_env()
+    else:
+        config = config_ini()
+
+    conn = psycopg2.connect(**config)
+    if conn:
+        return conn
+    else:
+        raise Exception("Connection failed, check your credentials")
+
+
+if __name__ == "__main__":
+    conn = connect()
+
+    # get postgresql version
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print(f"Connected to {record}")
+
+    # close the connection
+    cursor.close()
+    conn.close()
