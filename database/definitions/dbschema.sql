019b2648b6de01cb92ab044069a91fdb892ab765
matined <nedzimateusz@gmail.com>
2023-04-13 00:12:55 +0200

    initial db table definitions

--- database/definitions/dbschema.sql
+++ database/definitions/dbschema.sql
@@ -1,10 +1,12 @@
 -- IMPORTANT!!: When editing, compare this file with the db/dbdelete.sql file
 
+CREATE SCHEMA core;
+
 CREATE TYPE STATUS AS ENUM ('active', 'inactive');
 
 CREATE TYPE ROLE AS ENUM ('owner', 'admin', 'reader', 'writer');
 
-CREATE TABLE model (
+CREATE TABLE core.model (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
@@ -14,75 +16,75 @@ CREATE TABLE model (
     updated_by INT NOT NULL,
     image_tag VARCHAR(255),
     source_path VARCHAR(255),
-    status STATUS NOT NULL,
+    status STATUS NOT NULL
 );
 
-CREATE TABLE test (
+CREATE TABLE core.test (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     created_by INT NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    updated_by INT NOT NULL,
+    updated_by INT NOT NULL
 );
 
-CREATE TABLE model_test (
+CREATE TABLE core.model_test (
     model_id INT NOT NULL,
     test_id INT NOT NULL,
     PRIMARY KEY (model_id, test_id),
-    FOREIGN KEY (model_id) REFERENCES model(id),
-    FOREIGN KEY (test_id) REFERENCES test(id),
+    FOREIGN KEY (model_id) REFERENCES core.model(id),
+    FOREIGN KEY (test_id) REFERENCES core.test(id)
 );
 
-CREATE TABLE pool (
+CREATE TABLE core.pool (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     created_by INT NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    updated_by INT NOT NULL,
+    updated_by INT NOT NULL
 );
 
-CREATE TABLE pool_model (
+CREATE TABLE core.pool_model (
     pool_id INT NOT NULL,
     model_id INT NOT NULL,
     PRIMARY KEY (pool_id, model_id),
-    FOREIGN KEY (pool_id) REFERENCES pool(id),
-    FOREIGN KEY (model_id) REFERENCES model(id),
+    FOREIGN KEY (pool_id) REFERENCES core.pool(id),
+    FOREIGN KEY (model_id) REFERENCES core.model(id)
 );
 
-CREATE TABLE user (
+CREATE TABLE core.user (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-)
+    email VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE model_user_role (
+CREATE TABLE core.model_user_role (
     model_id INT NOT NULL,
     user_id INT NOT NULL,
     role ROLE NOT NULL,
     PRIMARY KEY (model_id, user_id),
-    FOREIGN KEY (model_id) REFERENCES model(id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (model_id) REFERENCES core.model(id),
+    FOREIGN KEY (user_id) REFERENCES core.user(id)
 );
 
-CREATE TABLE test_user_role (
+CREATE TABLE core.test_user_role (
     test_id INT NOT NULL,
     user_id INT NOT NULL,
     role ROLE NOT NULL,
     PRIMARY KEY (test_id, user_id),
-    FOREIGN KEY (test_id) REFERENCES test(id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (test_id) REFERENCES core.test(id),
+    FOREIGN KEY (user_id) REFERENCES core.user(id)
 );
 
-CREATE TABLE pool_user_role (
+CREATE TABLE core.pool_user_role (
     pool_id INT NOT NULL,
     user_id INT NOT NULL,
     role ROLE NOT NULL,
     PRIMARY KEY (pool_id, user_id),
-    FOREIGN KEY (pool_id) REFERENCES pool(id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (pool_id) REFERENCES core.pool(id),
+    FOREIGN KEY (user_id) REFERENCES core.user(id)
 );
