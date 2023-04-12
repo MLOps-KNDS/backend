046f1e8b29b95196f1ef77fa5b6bfa499d933d1c
Mateusz Nędzi <nedzimateusz@gmail.com>
2023-04-12 21:03:45 +0200

    Feature/docker compose (#15)
    * refactoring
    
    * db added to docker-compose

--- database/scripts/dbshow.py
+++ database/scripts/dbshow.py
@@ -12,17 +12,21 @@ async def show_db(conn: psycopg2.extensions.connection) -> dict:
     :rtype: dict
     """
     cursor = conn.cursor()
-    cursor.execute("""SELECT *
+    cursor.execute(
+        """SELECT *
                     FROM pg_catalog.pg_tables
                     WHERE schemaname != 'pg_catalog' AND
-                        schemaname != 'information_schema';""")
+                        schemaname != 'information_schema';"""
+    )
     records = cursor.fetchall()
     cursor.close()
     return {"status": "ok", "data": records}
 
+
 if __name__ == "__main__":
     import asyncio
     import dbconnect
+
     conn = dbconnect.connect()
     response = asyncio.run(show_db(conn))
     conn.close()
