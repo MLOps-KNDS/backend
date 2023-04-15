5dbcd832cee123375526438e98baf7bbdef6aa92
Mateusz Nedzi <nedzimateusz@gmail.com>
2023-04-15 13:47:27 +0200

    formatting

--- api/src/utils/image_builder.py
+++ api/src/utils/image_builder.py
@@ -4,6 +4,7 @@ import mlflow
 
 from src.utils.constants import Constants
 
+logging.basicConfig(level=logging.DEBUG)
 _logger = logging.getLogger(__name__)
 
 
@@ -36,17 +37,14 @@ class ImageBuilder:
         """
         try:
             _logger.info(f"Building a docker image with name {self.name}...")
-            mlflow.models.build_docker(
-                model_uri=self.model_uri,
-                name=self.name,
-                env_manager=Constants.MLFLOW_ENV_MANAGER,
-            )
+            self.__build_mlflow_image()
             self.is_built = True
-            _logger.info(f"Building a docker image with name '{self.name}' finished.")
+            _logger.info(f"Building a docker image with name {self.name} finished.")
         except Exception as e:
             _logger.error(
                 f"Building a docker image with name {self.name} failed with error: {e}"
             )
+            raise e
 
     def push(self) -> str:
         """
@@ -57,25 +55,38 @@ class ImageBuilder:
         if not self.is_built:
             raise Exception("Image is not built yet.")
         try:
-            _logger.info(f"Pushing a docker image with name '{self.name}'...")
-            client = docker.from_env()
-            client.login(
-                username="_json_key",
-                password=Constants.GCP_CREDENTIALS,
-                registry=Constants.GCP_CONTAINER_REGISTRY_URI,
-            )
-            image_tag = f"{Constants.GCP_CONTAINER_REGISTRY_URI}/{self.name}"
-            client.images.get(self.name).tag(image_tag)
-            for line in client.images.push(
-                repository=image_tag,
-                stream=True,
-                decode=True,
-            ):
-                _logger.debug(line)
-            _logger.info(f"Pushing a docker image with name '{self.name}'...")
+            _logger.info(f"Pushing a docker image with name {self.name}...")
+            image_tag = self.__push_image_to_gcr()
+            _logger.info(f"Pushing a docker image with name {self.name} finished.")
             return image_tag
         except Exception as e:
             _logger.error(
-                f"Pushing a docker image with name '{self.name}' failed with error: {e}"
+                f"Pushing a docker image with name {self.name} failed with error: {e}"
             )
             raise e
+
+    def __build_mlflow_image(self) -> None:
+        mlflow.models.build_docker(
+            model_uri=self.model_uri,
+            name=self.name,
+            env_manager=Constants.MLFLOW_ENV_MANAGER,
+        )
+    
+    def __push_image_to_gcr(self) -> str:
+        client = docker.from_env()
+        client.login(
+            username="_json_key",
+            password=Constants.GCP_CREDENTIALS,
+            registry=Constants.GCP_CONTAINER_REGISTRY_URI,
+        )
+        image_tag = f"{Constants.GCP_CONTAINER_REGISTRY_URI}/{self.name}"
+        client.images.get(self.name).tag(image_tag)
+
+        for line in client.images.push(
+            repository=image_tag,
+            stream=True,
+            decode=True,
+        ):
+            _logger.debug(line)
+            
+        return image_tag
