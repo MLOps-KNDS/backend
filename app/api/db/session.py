835816c0440d03d3211f4b4b71ad756607c661c5
HermanPlay <hermanplay1110@gmail.com>
2023-04-17 00:00:17 +0200

    Refactored and finished db module

--- app/api/db/session.py
+++ app/api/db/session.py
@@ -1,53 +1,12 @@
-from sqlalchemy import create_engine, text
+"""
+This module defines SessionLocal which may be imported for creating a
+database session
+"""
+
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
-from db_config import config_env, config_ini
 
+from config.config import settings
 
-def get_url() -> str:
-    """
-    Generates the url for connection
-
-    :return: Database connection url
-    """
-
-    env_vars_found = True
-    env_vars = [
-        "POSTGRES_HOST",
-        "POSTGRES_PORT",
-        "POSTGRES_NAME",
-        "POSTGRES_USER",
-        "POSTGRES_PASS",
-    ]
-    for var in env_vars:
-        if os.environ.get(var) is None:
-            print(f"Enviroment var {var} not set, looking for database.ini")
-            env_vars_found = False
-            break
-    if env_vars_found:
-        config = config_env()
-    else:
-        config = config_ini()
-
-    return "postgresql://{}:{}@{}:{}/{}".format(
-        config["user"],
-        config["password"],
-        config["host"],
-        config["port"],
-        config["dbname"],
-    )
-
-
-engine = create_engine(get_url(), pool_pre_ping=True)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-if __name__ == "__main__":
-    conn = create_engine(get_url(), pool_pre_ping=True).connect()
-
-    # get postgresql version
-    record = conn.execute(text("SELECT version();")).fetchone()
-    # record = cursor.fetchone()
-    print(f"Connected to {record}")
-
-    # close the connection
-    conn.close()
