0c4ca736f5b7ae9936d5e1758215c3d125122f6e
TheRealSeber <111927572+TheRealSeber@users.noreply.github.com>
2023-04-16 20:24:10 +0200

    Integration with SQLAlchemy ORM (#23)
    * Integration with SQLAlchemy ORM
    
    * password env fix
    
    * Refractor ORM models
    
    * Fix Black flake8
    
    * Removed SQL schemas, changed connection to Session
    
    * Fix docker compose
    
    * Defined structure, work in progres
    
    * Remove database folder
    
    ---------
    
    Co-authored-by: HermanPlay <hermanplay1110@gmail.com>

---
+++ api/src/models/model.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, Enum, DateTime, ForeignKey
+from base_class import Base
+
+from enum import Enum as En
+
+
+class StatusEnum(En):
+    active = "active"
+    inactive = "inactive"
+
+
+class Model(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    created_by = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    updated_at = Column(DateTime, nullable=False)
+    updated_by = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    image_tag = Column(String(255))
+    source_path = Column(String(255))
+    status = Column(Enum(StatusEnum), nullable=False)
+
+
+class ModelTest(Base):
+    id = Column(Integer, primary_key=True)
+    model_id = Column(Integer, ForeignKey("model.id"), primary_key=True)
+    test_id = Column(Integer, ForeignKey("test.id"), primary_key=True)
---
+++ api/src/models/test.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from base_class import Base
+
+
+class Test(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    created_by = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    updated_at = Column(DateTime, nullable=False)
+    updated_by = Column(Integer, ForeignKey("user.id"), primary_key=True)
