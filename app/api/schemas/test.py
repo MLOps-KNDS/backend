463ea88a623a3c668a733da8496f366ec603ff00
Mateusz Nedzi <nedzimateusz@gmail.com>
2023-04-18 23:16:44 +0200

    changes

--- app/api/schemas/test.py
+++ app/api/schemas/test.py
@@ -3,14 +3,14 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 
 
-class Test(BaseModel):
-    id: Annotated[int, Field(description="Unique ID")]
+class CreateTest(BaseModel):
     name: Annotated[str, Field(description="Test name")]
     description: Annotated[str, Field(description="Test description")]
-    created_at: Annotated[datetime, Field(description="Test creation date")]
     created_by: Annotated[int, Field(description="User ID of the creator")]
-    updated_at: Annotated[datetime, Field(description="Test update date")]
-    updated_by: Annotated[int, Field(description="User ID of the last updater")]
 
-    class Config:
-        orm_mode = True
+
+class UpdateTest(BaseModel):
+    id: Annotated[int, Field(description="Test ID")]
+    name: Annotated[str, Field(description="Test name")]
+    description: Annotated[str, Field(description="Test description")]
+    updated_by: Annotated[int, Field(description="User ID of the last updater")]
