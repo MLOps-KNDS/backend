fc8abbf7e84c9174c07a27c23d3b564be903ed27
Mateusz Nedzi <nedzimateusz@gmail.com>
2023-04-18 23:24:25 +0200

    flake resolved

--- app/api/schemas/model.py
+++ app/api/schemas/model.py
@@ -1,6 +1,5 @@
 from typing import Annotated
 from enum import Enum
-from datetime import datetime
 from pydantic import BaseModel, Field
 
 
--- app/api/schemas/test.py
+++ app/api/schemas/test.py
@@ -1,6 +1,5 @@
 from typing import Annotated
 from pydantic import BaseModel, Field
-from datetime import datetime
 
 
 class CreateTest(BaseModel):
