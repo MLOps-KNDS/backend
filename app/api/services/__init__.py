3cff1bd7d7e14c4bba47004d61e0ffe9f2a71291
Mateusz Nedzi <nedzimateusz@gmail.com>
2023-04-22 21:17:38 +0200

    service and endpopint for pools

--- app/api/services/__init__.py
+++ app/api/services/__init__.py
@@ -1,2 +1,2 @@
-from services.deps import get_db # noqa: F401
-from services.pool import PoolService # noqa: F401
+from services.deps import get_db  # noqa: F401
+from services.pool import PoolService  # noqa: F401
