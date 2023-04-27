17f0353d8d4107ad7bc13079f5d0e2d6253f6406
TheRealSeber <rydzseba@gmail.com>
2023-04-27 23:04:05 +0200

    Complete rewrite

---
+++ app/api/tests/db/session.py
@@ -0,0 +1,71 @@
+"""
+This module provides a function to create a test session with a TestClient
+that can be used to test FastAPI endpoints.
+
+More: https://stackoverflow.com/questions/67255653/
+how-to-set-up-and-tear-down-a-database-between-tests-in-fastapi
+"""
+
+from fastapi.testclient import TestClient
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import pytest
+
+from main import app
+from services.deps import get_db
+import models
+from ..config.config import settings
+
+engine = sqlalchemy.create_engine(
+    settings.TEST_SQLALCHEMY_DATABASE_URI, pool_pre_ping=True
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Set up the database once
+models.Base.metadata.drop_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
+
+
+# It creates a nested
+# transaction, recreates it when the application code calls session.commit
+# and rolls it back at the end.
+# Based on: https://docs.sqlalchemy.org/en/14/orm/session_transaction
+# .html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
+@pytest.fixture()
+def session():
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
+
+    # Begin a nested transaction (using SAVEPOINT).
+    nested = connection.begin_nested()
+
+    # If the application code calls session.commit, it will end the nested
+    # transaction. Need to start a new one when that happens.
+    @sqlalchemy.event.listens_for(session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        nonlocal nested
+        if not nested.is_active:
+            nested = connection.begin_nested()
+
+    yield session
+
+    # Rollback the overall transaction, restoring the state before the test ran.
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+# A fixture for the fastapi test client which depends on the
+# previous session fixture. Instead of creating a new session in the
+# dependency override as before, it uses the one provided by the
+# session fixture.
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        yield session
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    del app.dependency_overrides[get_db]
--- app/api/tests/test_user.py
+++ app/api/tests/test_user.py
@@ -1,46 +1,48 @@
 """
 Integration module for the user endpoints.
 
-For each of the endpoints, this function makes a request to the API
-using the TestClient, validates that the response and the expected output.
+For each of the endpoints, given function makes a request to the API
+using the "client", validates that the response and the expected output.
 
 Note that this function requires a database session to be created prior to
 running. The session can be created by running the `create_test_session`
 function from the `test_db.session` module.
+
+This test file covers the following endpoints:
+- PUT /user/: creates a new user with the given information.
+- PATCH /user/{user_id}: updates an existing user with the given information.
+- GET /user/{user_id}: retrieves an existing user with the given ID.
+- GET /user/: retrieves a list of all existing users.
+- DELETE /user/{user_id}: deletes an existing user with the given ID.
+
+This function assumes that the Database is running and accessible at the URL,
+set on the test_config.config.settings.
 """
 
 import schemas.user as user_schemas
-from .test_db.session import create_test_session
+from .db.session import client, session
 
-client = create_test_session()
 
 ROUTE = "/user/"
 
 
-def test_user():
-    """
-    This test function covers the following endpoints:
-    - PUT /user/: creates a new user with the given information.
-    - PATCH /user/{user_id}: updates an existing user with the given information.
-    - GET /user/{user_id}: retrieves an existing user with the given ID.
-    - GET /user/: retrieves a list of all existing users.
-    - DELETE /user/{user_id}: deletes an existing user with the given ID.
-
-    This function assumes that the API is running and accessible at the URL,
-    set on the test_config.config.settings.
-    """
-
-    # put_user
+def test_user_put(client):
     test_user = user_schemas.UserPut(
         name="test_name", surname="test_surname", email="test_email@abc.com"
     )
     response = client.put(ROUTE, json=dict(test_user))
     assert response.status_code == 200
-    data = response.json()
-    assert "id" in data
-    user_id = data["id"]
+    assert "id" in response.json()
 
-    # patch_user
+
+def test_user_patch(client):
+    test_user = user_schemas.UserPut(
+        name="test_name", surname="test_surname", email="test_email@abc.com"
+    )
+    response = client.put(ROUTE, json=dict(test_user))
+    assert response.status_code == 200
+    user_id = response.json()["id"]
+
     test_user_patch = user_schemas.UserPatch(
         name="test_patch_name",
         surname="test_patch_surname",
@@ -56,22 +58,53 @@ def test_user():
         "email": "test_patch_email@abc.com",
     }
 
-    # get_user
-    response = client.get(f"{ROUTE}{user_id}")
-    assert response.status_code == 200
-    assert response.json() == data
 
-    # get_users
+def test_user_get(client):
+    test_user = user_schemas.UserPut(
+        name="test_name", surname="test_surname", email="test_email@abc.com"
+    )
     response = client.put(ROUTE, json=dict(test_user))
     assert response.status_code == 200
-    params = {"skip": 0, "limit": 100}
+    user_id = response.json()["id"]
+
+    response = client.get(f"{ROUTE}{user_id}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": user_id,
+        "name": "test_name",
+        "surname": "test_surname",
+        "email": "test_email@abc.com",
+    }
+
+
+def test_users_get(client):
+    test_user_1 = user_schemas.UserPut(
+        name="test_name", surname="test_surname", email="test_email_1@abc.com"
+    )
+    response_user_1 = client.put(ROUTE, json=dict(test_user_1))
+    assert response_user_1.status_code == 200
+
+    test_user_2 = user_schemas.UserPut(
+        name="test_name", surname="test_surname", email="test_email_2@abc.com"
+    )
+    response_user_2 = client.put(ROUTE, json=dict(test_user_2))
+    assert response_user_2.status_code == 200
+
+    params = {"skip": 0, "limit": 3}
     response = client.get(ROUTE, params=params)
     assert response.status_code == 200
     data = response.json()
-    assert len(data) > 1
+    assert data == [response_user_1.json(), response_user_2.json()]
 
-    # delete_users
-    for model in data:
-        del_response = client.delete(f"{ROUTE}{model['id']}")
-        assert del_response.status_code == 200
-        assert del_response.json() == {"detail": "success"}
+
+def test_user_delete(client):
+    test_user = user_schemas.UserPut(
+        name="test_name", surname="test_surname", email="test_email@abc.com"
+    )
+    response = client.put(ROUTE, json=dict(test_user))
+    assert response.status_code == 200
+    user_id = response.json()["id"]
+
+    del_response = client.delete(f"{ROUTE}{user_id}")
+    assert del_response.status_code == 200
+    assert del_response.json() == {"detail": "success"}
