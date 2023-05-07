81bd6eb88aa8a87bb31de34d1086df50a8034045
TheRealSeber <rydzseba@gmail.com>
2023-05-07 12:47:18 +0200

    schema creation fix

--- app/api/tests/conftest.py
+++ app/api/tests/conftest.py
@@ -1,18 +1,22 @@
 import pytest
 import sqlalchemy
-from .config.config import settings
 import models
-
-engine = sqlalchemy.create_engine(
-    settings.TEST_SQLALCHEMY_DATABASE_URI, pool_pre_ping=True
-)
+from .db.session import engine
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup():
+    with engine.connect() as conn:
+        conn.execute(sqlalchemy.text("CREATE SCHEMA IF NOT EXISTS core"))
+        conn.commit()
+
     # Set up the database
     models.Base.metadata.drop_all(bind=engine)
     models.Base.metadata.create_all(bind=engine)
     yield
     # Teardown the database
     models.Base.metadata.drop_all(bind=engine)
+
+    with engine.connect() as conn:
+        conn.execute(sqlalchemy.text("DROP SCHEMA IF EXISTS core"))
+        conn.commit()
