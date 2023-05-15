3299f2f6adb1c047a8b03a6b4eecbc5ec354760d
matined <nedzimateusz@gmail.com>
2023-05-15 20:47:26 +0200

    changes

--- app/api/services/model.py
+++ app/api/services/model.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 from models import model as model_models
 from schemas import model as model_schemas
-from utils.model_builder import ModelBuilder
 
 
 class ModelService:
@@ -108,5 +107,4 @@ class ModelService:
         """
         db.query(model_models.Model).filter(model_models.Model.id == model_id).delete()
         db.commit()
-        
         return JSONResponse({"detail": "model deleted"})
