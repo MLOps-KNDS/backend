edaafd3680d25d0928ad71d5f291e79027f9a3a4
HermanPlay <hermanplay1110@gmail.com>
2023-05-22 14:09:39 +0200

    Almost working

--- app/api/main.py
+++ app/api/main.py
@@ -3,9 +3,14 @@ This is the main module.
 It contains the FastAPI app.
 """
 
-from fastapi import FastAPI
-from contextlib import asynccontextmanager
 import time
+from fastapi import FastAPI, Depends, Request
+from fastapi.exceptions import HTTPException
+from contextlib import asynccontextmanager
+from starlette.config import Config
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import RedirectResponse
+from authlib.integrations.starlette_client import OAuth
 
 from db.create import create_db
 from db.session import engine
@@ -33,11 +38,29 @@ app = FastAPI(
     lifespan=init,
 )
 
+app.add_middleware(SessionMiddleware, secret_key="secret-string")
+
+config = Config('config.env')  # read config from .env file
+oauth = OAuth(config)
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
+
 app.include_router(model.router)
 app.include_router(user.router)
 app.include_router(pool.router)
 app.include_router(gate.router)
 app.include_router(test.router)
+
+async def get_current_user(request: Request):
+    if request.session.get('user'):
+        return request.session['user']
+    raise HTTPException(401, 'Unauthorized')
+    
 
 
 @app.get("/")
@@ -47,3 +70,29 @@ async def root() -> dict:
     :return: A "Hello World" message.
     """
     return {"message": "Hello World"}
+
+@app.get('/me')
+async def me(request: Request):
+    user = await get_current_user(request)
+    return user
+
+@app.get('/login')
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')
+    return await oauth.google.authorize_redirect(request, str(redirect_uri))
+
+
+@app.get('/auth')
+async def auth(request: Request):
+    token = await oauth.google.authorize_access_token(request)
+    user = token.get('userinfo')
+    print(user)
+    if user:
+        request.session['user'] = dict(user)
+    return RedirectResponse(url='/me')
+
+
+@app.get('/logout')
+async def logout(request: Request):
+    request.session.pop('user', None)
+    return RedirectResponse(url='/')
