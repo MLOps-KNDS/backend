77c5a6f15a8871515bbd451fdc88e1c221e76189
matined <nedzimateusz@gmail.com>
2023-05-28 11:48:51 +0200

    Updated constants.py

--- app/api/utils/constants.py
+++ app/api/utils/constants.py
@@ -4,8 +4,8 @@ import base64
 
 class Constants:
     # Google Cloud
-    GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "NOTSET==")
-    GCP_CREDENTIALS_ENCODED = os.environ.get("GCP_CREDENTIALS_ENCODED", "NOTSET==")
+    GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "RFVNTVk=")
+    GCP_CREDENTIALS_ENCODED = os.environ.get("GCP_CREDENTIALS_ENCODED", "RFVNTVk=")
     GCP_CREDENTIALS = base64.b64decode(GCP_CREDENTIALS_ENCODED).decode("utf-8")
 
     GCP_CONTAINER_REGISTRY_URI = f"gcr.io/{GCP_PROJECT_ID}"
