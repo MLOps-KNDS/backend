13a8ec95ef08d7f7a826b25cb5d40359745e07b0
Sebastian Rydz <111927572+TheRealSeber@users.noreply.github.com>
2023-05-28 10:12:21 +0200

    Feature/pool model endpoints hotfix (#70)
    * Added tests for the user router
    
    * pool_model_hotfix
    
    * pool_model_hotfix
    
    * tag name changed
    
    * Filip S is a god

--- app/api/routers/pool_model.py
+++ app/api/routers/pool_model.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from schemas import pool_model as pool_model_schemas
 from services import PoolService, ModelService, PoolModelService, get_db
 
-router = APIRouter(prefix="/pool", tags=["pool"])
+router = APIRouter(prefix="/pool", tags=["pool-model"])
 
 
 @router.get(
@@ -26,7 +26,7 @@ async def get_pool_models(pool_id: int, db: Session = Depends(get_db)):
     """
     if not PoolService.get_pool_by_id(db=db, id=pool_id):
         raise HTTPException(status_code=404, detail="Pool not found!")
-    return PoolModelService.get_pool_models(db=db, id=pool_id)
+    return PoolModelService.get_pool_models(db=db, pool_id=pool_id)
 
 
 @router.put(
@@ -35,11 +35,16 @@ async def get_pool_models(pool_id: int, db: Session = Depends(get_db)):
     status_code=201,
 )
 async def put_pool_model(
-    pool_model_data: pool_model_schemas.PoolPutModel, db: Session = Depends(get_db)
+    pool_id: int,
+    model_id: int,
+    pool_model_data: pool_model_schemas.PoolPutModel,
+    db: Session = Depends(get_db),
 ):
     """
     Inserts a model into a given pool.
 
+    :param pool_id: the pool ID to retrieve models from
+    :param model_id: the model ID to retrieve from the pool
     :param pool_model_data: the information about the pool and model.
     :param db: Database session
 
@@ -47,14 +52,22 @@ async def put_pool_model(
     if the specified pool ID does not exist in the database.
     :raise HTTPException: 404 status code with "Model not found!" message
     if the specified model ID does not exist in the database.
+    :raise HTTPException: 404 status code with "Model not found!" message
+    if the specified pool ID does not exist in the database.
 
     :return: the newly-inserted pool record
     """
-    if not PoolService.get_pool_by_id(db=db, id=pool_model_data.pool_id):
+    if not PoolService.get_pool_by_id(db=db, id=pool_id):
         raise HTTPException(status_code=404, detail="Pool not found!")
-    if not ModelService.get_model_by_id(db=db, model_id=pool_model_data.model_id):
+    if not ModelService.get_model_by_id(db=db, model_id=model_id):
         raise HTTPException(status_code=404, detail="Model not found!")
-    return PoolModelService.put_pool_model(db=db, data=pool_model_data)
+    if PoolModelService.get_pool_model_by_model_id(db=db, id=model_id):
+        raise HTTPException(
+            status_code=404, detail="Model already registered in the pool!"
+        )
+    return PoolModelService.put_pool_model(
+        db=db, pool_id=pool_id, model_id=model_id, data=pool_model_data
+    )
 
 
 @router.patch(
@@ -63,6 +76,8 @@ async def put_pool_model(
     status_code=200,
 )
 async def patch_pool_model(
+    pool_id: int,
+    model_id: int,
     pool_model_data: pool_model_schemas.PoolPatchModel,
     db: Session = Depends(get_db),
 ):
@@ -70,6 +85,8 @@ async def patch_pool_model(
     Updates the information of an existing pool with the provided data and
     returns the updated pool information.
 
+    :param pool_id: the pool ID to retrieve models from
+    :param model_id: the model ID to retrieve from the pool
     :param pool_model_data: the information of the model in the pool to be patched.
     :param db: Database session
 
@@ -82,24 +99,24 @@ async def patch_pool_model(
 
     :return: the updated pool record
     """
-    if not PoolService.get_pool_by_id(db=db, id=pool_model_data.pool_id):
+    if not PoolService.get_pool_by_id(db=db, id=pool_id):
         raise HTTPException(status_code=404, detail="Pool not found!")
-    if not ModelService.get_model_by_id(db=db, model_id=pool_model_data.model_id):
+    if not ModelService.get_model_by_id(db=db, model_id=model_id):
         raise HTTPException(status_code=404, detail="Model not found!")
-    if not PoolModelService.get_pool_model_by_model_id(
-        db=db, id=pool_model_data.model_id
-    ):
+    if not PoolModelService.get_pool_model_by_model_id(db=db, id=model_id):
         raise HTTPException(status_code=404, detail="Model not found in the pool!")
-    return PoolModelService.patch_pool_model(db=db, data=pool_model_data)
+    return PoolModelService.patch_pool_model(
+        db=db, pool_id=pool_id, model_id=model_id, data=pool_model_data
+    )
 
 
 @router.delete("/{pool_id}/model/{model_id}", status_code=200)
-async def delete_pool_model(
-    pool_model_data: pool_model_schemas.PoolDeleteModel, db: Session = Depends(get_db)
-):
+async def delete_pool_model(pool_id: int, model_id: int, db: Session = Depends(get_db)):
     """
     Deletes the pool with the given ID.
 
+    :param pool_id: the pool ID to retrieve models from
+    :param model_id: the model ID to retrieve from the pool
     :param pool_model_data: the information of the model in the pool to be deleted.
     :param db: Database session
 
@@ -107,11 +124,15 @@ async def delete_pool_model(
     if the specified pool ID does not exist in the database.
     :raise HTTPException: 404 status code with "Model not found!" message
     if the specified pool ID does not exist in the database.
+    :raise HTTPException: 404 status code with "Model not found!" message
+    if the specified pool ID does not exist in the database.
 
     :return: a json with a "detail" key indicating success
     """
-    if not PoolService.get_pool_by_id(db=db, id=pool_model_data.pool_id):
+    if not PoolService.get_pool_by_id(db=db, id=pool_id):
         raise HTTPException(status_code=404, detail="Pool not found!")
-    if not ModelService.get_model_by_id(db=db, model_id=pool_model_data.model_id):
+    if not ModelService.get_model_by_id(db=db, model_id=model_id):
         raise HTTPException(status_code=404, detail="Model not found!")
-    return PoolModelService.delete_pool_model(db=db, data=pool_model_data)
+    if not PoolModelService.get_pool_model_by_model_id(db=db, id=model_id):
+        raise HTTPException(status_code=404, detail="Model not found in the pool!")
+    return PoolModelService.delete_pool_model(db=db, model_id=model_id, pool_id=pool_id)
